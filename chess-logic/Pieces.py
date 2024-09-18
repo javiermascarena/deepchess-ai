25a6805925ede3f680b2bef434de2ec799f401c1
javiermascarena <javiermascarenagonzalez@gmail.com>
2024-09-18 19:52:53 +0200

    Added the rest types of pieces
    Calculated possible moves for all of them except for rook, bishop and queen. Where we have yet to do a system so that the piece cannot "jump over" another piece.

--- chess-logic/Pieces.py
+++ chess-logic/Pieces.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class Piece: 
 
-    def __init__(self, position: list[int, int], color: bool):
+    def __init__(self, position: list[int], color: bool):
         """Initialization of variables"""
 
         # Stores the current position of the piece in the board
@@ -12,38 +12,54 @@ class Piece:
         # List that will store the possible moves of the piece
         self.possible_moves = []
 
-    def check_inboard(self, position: list[int, int]) -> bool:
+
+    def check_inboard(self, position: list[int]) -> bool:
         """Checks whether a position is inside the board or not"""
         return 0 <= position[0] <= 7 and 0 <= position[1] <= 7
+    
+
+    def delete_outboard(self, positions: list) -> list: 
+        """Returns the position of a list of positions that are in the board"""
+        inboard_positions = []
+
+        # Checking if each position is inboard
+        for pos in positions: 
+            if self.check_inboard(pos):
+                inboard_positions.append(pos)
+
+        return inboard_positions
+    
+    def reset_posible_moves(self) -> None:
+        """Resets possible moves of a piece, which will be done each turn"""
+        self.possible_moves = []
+
 
 
 class Pawn(Piece):
 
-    def __init__(self, position: list[int, int], color: bool):
-        super().__init__(position, color)
-
-    
-    def possible_moves(self, board: list) -> None:
+    def calculate_possible_moves(self, board: list) -> None:
         """Calculates the possible moves the pawn can make"""
 
         # When the pawn is black it will move to a higher row in the board,
         # and when it is white to a lower one
         forward = -1 if self.color else 1
-
+        
         # Checking the move to go 1 tile forward
         pos_check = [self.position[0], self.position[1] + forward]
-   
+        piece = board[pos_check[0]][pos_check[1]]
+
         # Can only fo 1 tile forward if there is no other piece in that tile and 
         # it is not out of limits
-        if board[pos_check[0], pos_check[1]] == 0 and self.check_inboard(pos_check):
+        if piece == 0 and self.check_inboard(pos_check):
             self.possible_moves.append(pos_check)
 
         # Checking the move to go 2 tiles forward
         pos_check = [self.position[0], self.position[1] + 2*forward]
 
+        piece = board[pos_check[0]][pos_check[1]]
         # Can only go 2 tiles forward if it is in the beginning position, and there 
         # is no other piece in that tile, and it is not out of limits
-        if board[pos_check[0], pos_check[1]] == 0 and self.check_inboard(pos_check) and \
+        if piece == 0 and self.check_inboard(pos_check) and \
               ((self.position[0] == 6 and self.color) or (self.position[0] == 1 and not self.color)):
             self.possible_moves.append(pos_check)
 
@@ -51,12 +67,156 @@ class Pawn(Piece):
         pos_check = [[self.position[0] + 1, self.position[1] + forward], 
                      [self.position[0] - 1, self.position[1] + forward]]
         for pos in pos_check: 
+            piece = board[pos[0]][pos[1]]
 
             # Can only go diagonally if there is one piece there and it is not out limits
-            if board[pos[0], pos[1]] != 0 and board[pos[0], pos[1]].color != self.color and \
+            if piece != 0 and piece.color != self.color and \
                   self.check_inboard(pos):
                 self.possible_moves.append(pos)
 
 
     def __repr__(self) -> str: 
         return "p" if self.color else "P"
+    
+
+
+class Rook(Piece):
+
+    def calculate_possible_moves(self, board: list) -> None:
+        """Calculates the possible moves the rook can make"""
+
+        pos_check = []
+
+        # All possible rook moves
+        for i in range(8):
+            pos_check.append([self.position[0], self.position[1] + i])
+            pos_check.append([self.position[0], self.position[1] - i])
+            pos_check.append([self.position[0] + i, self.position[1]])
+            pos_check.append([self.position[0] - i, self.position[1]])
+
+        # Removing outboard moves
+        pos_check = self.delete_outboard(pos_check)
+
+
+    def __repr__(self) -> str: 
+        return "r" if self.color else "R"
+    
+
+
+class Bishop(Piece):
+
+    def calculate_possible_moves(self, board: list) -> None:
+        """Calculates the possible moves the bishop can make"""
+
+        pos_check = []
+
+        # All posible bishop moves
+        for i in range(8):
+            pos_check.append([self.position[0] + i, self.position[1] + i])
+            pos_check.append([self.position[0] - i, self.position[1] + i])
+            pos_check.append([self.position[0] + i, self.position[1] - i])
+            pos_check.append([self.position[0] - i, self.position[1] - i])
+
+        # Removing outboard moves
+        pos_check = self.delete_outboard(pos_check)
+
+
+    def __repr__(self) -> str: 
+        return "b" if self.color else "B"
+
+
+
+class Knight(Piece):
+    
+    def calculate_possible_moves(self, board: list) -> None:
+        """Calculates the possible moves the knight can make"""
+
+        pos_check = []
+
+        # All possible knight moves
+        pos_check.append([self.position[0] + 2, self.position[1] + 1])
+        pos_check.append([self.position[0] + 2, self.position[1] - 1])
+        pos_check.append([self.position[0] - 2, self.position[1] + 1])
+        pos_check.append([self.position[0] - 2, self.position[1] - 1])
+        pos_check.append([self.position[0] + 1, self.position[1] + 2])
+        pos_check.append([self.position[0] - 1, self.position[1] + 2])
+        pos_check.append([self.position[0] + 1, self.position[1] - 2])
+        pos_check.append([self.position[0] - 1, self.position[1] - 2])
+
+        # Deleting the moves that are outside the board
+        pos_check = self.delete_outboard(pos_check)
+
+        # Checking every position 
+        for pos in pos_check: 
+            piece = board[pos[0]][pos[1]] 
+
+            # The movement can be done if there is no piece 
+            # in the tile or the piece is of the opposite color
+            if piece == 0 or piece.color is not self.color: 
+                self.possible_moves.append(pos)
+
+
+    def __repr__(self) -> str:
+        return "k" if self.color else "K"
+    
+
+
+class Queen(Piece):
+
+    def calculate_possible_moves(self, board: list) -> None:
+        """Calculates the possible moves the queen can make"""
+
+        pos_check = []
+        for i in range(8):
+            # Rook type moves
+            pos_check.append([self.position[0], self.position[1] + i])
+            pos_check.append([self.position[0], self.position[1] - i])
+            pos_check.append([self.position[0] + i, self.position[1]])
+            pos_check.append([self.position[0] - i, self.position[1]])
+            # Bishop type moves
+            pos_check.append([self.position[0] + i, self.position[1] + i])
+            pos_check.append([self.position[0] - i, self.position[1] + i])
+            pos_check.append([self.position[0] + i, self.position[1] - i])
+            pos_check.append([self.position[0] - i, self.position[1] - i])
+
+        # Deleting outboard moves
+        pos_check = self.delete_outboard(pos_check)
+
+
+    def __repr__(self) -> str: 
+        return "r" if self.color else "R"
+
+
+
+class King(Piece): 
+
+     def calculate_possible_moves(self, board: list) -> None:
+        """Calculates the possible moves the king can make"""
+
+        pos_check = []
+
+        # All possible king moves
+        pos_check.append([self.position[0], self.position[1] + 1])
+        pos_check.append([self.position[0], self.position[1] - 1])
+        pos_check.append([self.position[0] + 1, self.position[1]])
+        pos_check.append([self.position[0] - 1, self.position[1]])
+        pos_check.append([self.position[0] + 1, self.position[1] + 1])
+        pos_check.append([self.position[0] + 1, self.position[1] - 1])
+        pos_check.append([self.position[0] - 1, self.position[1] + 1])
+        pos_check.append([self.position[0] - 1, self.position[1] - 1])
+
+        # Deleting the moves that are outside the board
+        pos_check = self.delete_outboard(pos_check)
+
+        # Checking every position 
+        for pos in pos_check: 
+            piece = board[pos[0]][pos[1]] 
+
+            # The movement can be done if there is no piece 
+            # in the tile or the piece is of the opposite color
+            if piece == 0 or piece.color is not self.color: 
+                self.possible_moves.append(pos)
+
+    
+     def __repr__(self) -> str:
+        return "a" if self.color else "A"
